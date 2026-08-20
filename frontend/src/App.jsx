147906634c4a0b3eea762d94746d770e6b9f57bc
dopiejay <jaycapalot248@gmail.com>
2026-08-20 08:19:52 +0200

    Admin route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -40,7 +40,7 @@ export default function App() {
 
         {/* Admin portal */}
         <Route
-          path="/admin"
+          path="/admin/*"
           element={
             <AdminAuthProvider>
               <Routes>
